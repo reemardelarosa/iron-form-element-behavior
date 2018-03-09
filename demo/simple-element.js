8fdf5648930c9bbca7e3bf910014ab8cb6d763b4
Elliott Marquez <emarquez@google.com>
2018-03-08 16:08:34 -0800

    auto-converted by polymer-modulizer

--- demo/simple-element.js
+++ demo/simple-element.js
@@ -1,4 +1,8 @@
-<!--
+import '../../polymer/polymer.js';
+import { IronFormElementBehavior } from '../iron-form-element-behavior.js';
+import { Polymer } from '../../polymer/lib/legacy/polymer-fn.js';
+
+/**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
@@ -6,18 +10,15 @@ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
 The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
-<link rel="import" href="../../polymer/polymer.html">
-<link rel="import" href="../iron-form-element-behavior.html">
+*/
+Polymer({
 
-<script>
+  is: 'simple-element',
 
-  Polymer({
-    is: 'simple-element',
-    extends: 'input',
-    behaviors: [
-      Polymer.IronFormElementBehavior
-    ]
-  });
+  extends: 'input',
 
-</script>
+  behaviors: [
+    IronFormElementBehavior
+  ]
+
+});
--- test/simple-form.js
+++ test/simple-form.js
@@ -1,4 +1,7 @@
-<!--
+import '../../polymer/polymer.js';
+import { Polymer } from '../../polymer/lib/legacy/polymer-fn.js';
+
+/**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
@@ -6,22 +9,8 @@ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
 The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
-<link rel="import" href="../../polymer/polymer.html">
-<link rel="import" href="../iron-form-element-behavior.html">
-
-<script>
-
-  Polymer({
-
-    is: 'simple-element',
-
-    extends: 'input',
-
-    behaviors: [
-      Polymer.IronFormElementBehavior
-    ]
-
-  });
-
-</script>
+*/
+Polymer({
+  is: 'simple-form',
+  extends: 'form'
+});
